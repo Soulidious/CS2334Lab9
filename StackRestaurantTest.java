1667ba7c3176f29f999e7fa1eabb13fdd2346ff7
Joe <Joseph.Lucas-1@ou.edu>
2019-10-19 14:40:23 -0500

    Test cases created and completed

--- StackRestaurantTest.java
+++ StackRestaurantTest.java
@@ -23,6 +23,12 @@ public class StackRestaurantTest {
 	{
 		stack = new StackRestaurant<String>();
 		//TODO:implement this
+		Order<String> order1 = new Order<String>("bacon", 12);
+		Order<String> order2 = new Order<String>("sausage", 14);
+		stack.addOrder(order1);
+		Assert.assertEquals(order1, stack.getOrderList().get(0));
+		stack.addOrder(order2);
+		Assert.assertEquals(order2, stack.getOrderList().get(1));
 	}
 	/**
 	 * test completeOrder() using at least three orders
@@ -34,6 +40,16 @@ public class StackRestaurantTest {
 	{
 		stack = new StackRestaurant<String>();
 		//TODO: implement this
+		Order<String> order1 = new Order<String>("bacon", 12);
+		Order<String> order2 = new Order<String>("sausage", 14);
+		Order<String> order3 = new Order<String>("bacon", 11);
+		stack.addOrder(order1);
+		stack.addOrder(order2);
+		stack.addOrder(order3);
+		Assert.assertEquals(order3, stack.checkNextCompletedOrder());
+		stack.completeOrder();
+		Assert.assertEquals(order2, stack.checkNextCompletedOrder());
+		
 	}
 	/**
 	 * test numberRemainingOrder() with at least 2 orders
@@ -44,6 +60,16 @@ public class StackRestaurantTest {
 	{
 		stack = new StackRestaurant<String>();
 		//TODO: implement this
+		Order<String> order1 = new Order<String>("bacon", 12);
+		Order<String> order2 = new Order<String>("sausage", 14);
+		Order<String> order3 = new Order<String>("bacon", 11);
+		stack.addOrder(order1);
+		stack.addOrder(order2);
+		stack.addOrder(order3);
+		Assert.assertEquals(3, stack.numberRemainingOrder());
+		stack.completeOrder();
+		stack.completeOrder();
+		Assert.assertEquals(1, stack.numberRemainingOrder());
 	}
 	/**
 	 * test both cases of completeOrder
@@ -53,6 +79,16 @@ public class StackRestaurantTest {
 	{
 		stack = new StackRestaurant<String>();
 		//TODO: implement this
+		Order<String> order1 = new Order<String>("bacon", 12);
+		Order<String> order2 = new Order<String>("sausage", 14);
+		Order<String> order3 = new Order<String>("bacon", 11);
+		stack.addOrder(order1);
+		stack.addOrder(order2);
+		stack.addOrder(order3);
+		Assert.assertEquals(order3, stack.checkNextCompletedOrder());
+		stack.completeOrder();
+		stack.completeOrder();
+		Assert.assertEquals(order1, stack.checkNextCompletedOrder());
 	}
 }
 
